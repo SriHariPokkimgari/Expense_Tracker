e854d7176cbc14783118d5b0f02f220f35947d72
Sri Hari <pokkimgarisrihari5@gmail.com>
2026-05-15 17:52:56 +0530

    Update axios base url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `http://localhost:8000/api`,
+  baseURL: `https://expense-tracker-api-s060.onrender.com/api`,
   withCredentials: true,
 });
 
